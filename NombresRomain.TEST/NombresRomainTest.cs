e3ab393fdd93368ff55fdbfaaaa014998d974404
Paulcastaings <paul.castaings@epsi.fr>
2023-08-02 14:31:07 +0200

    test 4 rouge

--- NombresRomain.TEST/NombresRomainTest.cs
+++ NombresRomain.TEST/NombresRomainTest.cs
@@ -17,26 +17,39 @@ namespace NombresRomain.TEST
 
         [Fact]
         public void Test2()
-    {
-        // ETANT DONNE le chiffre 2
-        const uint chiffreArabe = 2;
+        {
+            // ETANT DONNE le chiffre 2
+            const uint chiffreArabe = 2;
 
-        // QUAND on le converti en nombres romains
-        var nombreRomain = ConvertNombresRomain.Convertir(chiffreArabe);
+            // QUAND on le converti en nombres romains
+            var nombreRomain = ConvertNombresRomain.Convertir(chiffreArabe);
 
-        // ALORS j'obtiens II
-        Assert.Equal("II", nombreRomain);
-    }[Fact]
+            // ALORS j'obtiens II
+            Assert.Equal("II", nombreRomain);
+        }
+        [Fact]
         public void Test3()
-    {
-        // ETANT DONNE le chiffre 3
-        const uint chiffreArabe = 3;
+        {
+            // ETANT DONNE le chiffre 3
+            const uint chiffreArabe = 3;
 
-        // QUAND on le converti en nombres romains
-        var nombreRomain = ConvertNombresRomain.Convertir(chiffreArabe);
+            // QUAND on le converti en nombres romains
+            var nombreRomain = ConvertNombresRomain.Convertir(chiffreArabe);
 
-        // ALORS j'obtiens III
-        Assert.Equal("III", nombreRomain);
+            // ALORS j'obtiens III
+            Assert.Equal("III", nombreRomain);
+        }
+        [Fact]
+        public void Test4()
+        {
+            // ETANT DONNE le chiffre 4
+            const uint chiffreArabe = 4;
+
+            // QUAND on le converti en nombres romains
+            var nombreRomain = ConvertNombresRomain.Convertir(chiffreArabe);
+
+            // ALORS j'obtiens IV
+            Assert.Equal("IV", nombreRomain);
+        }
     }
-}
 }
