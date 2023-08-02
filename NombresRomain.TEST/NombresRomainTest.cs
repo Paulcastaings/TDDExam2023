0eb720ef9cf77c99644c5075a66e91fa7dea7570
Paulcastaings <paul.castaings@epsi.fr>
2023-08-02 14:14:54 +0200

    test 2 rouge

--- NombresRomain.TEST/NombresRomainTest.cs
+++ NombresRomain.TEST/NombresRomainTest.cs
@@ -14,5 +14,18 @@ namespace NombresRomain.TEST
             // ALORS j'obtiens I
             Assert.Equal("I", nombreRomain);
         }
+
+        [Fact]
+        public void Test2()
+    {
+        // ETANT DONNE le chiffre 2
+        const uint chiffreArabe = 2;
+
+        // QUAND on le converti en nombres romains
+        var nombreRomain = ConvertNombresRomain.Convertir(chiffreArabe);
+
+        // ALORS j'obtiens I
+        Assert.Equal("II", nombreRomain);
     }
+}
 }
