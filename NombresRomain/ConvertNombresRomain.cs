43802e6a6e9707429dddad67822a52e495676c75
Paulcastaings <paul.castaings@epsi.fr>
2023-08-02 14:18:41 +0200

    test 2 vert

--- NombresRomain/ConvertNombresRomain.cs
+++ NombresRomain/ConvertNombresRomain.cs
@@ -3,8 +3,11 @@
     public class ConvertNombresRomain
     {
         public static string Convertir(uint chiffreArabe)
-        {
-            return "I";
-        }
+
+            => chiffreArabe switch
+            {
+                1 => "I",
+                _ => "II",
+            };
     }
 }
